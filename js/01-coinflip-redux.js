d02a78d7f1b6b3776fb80d5ba92f1ea992872337
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-25 21:55:18 -0800

    The “Coin Flip” Game Redux

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,12 @@
+let coinFlip
+coinFlip = parseInt(prompt("How many times do you want to flip the coin?"))
+
+for (var i = 0; i < coinFlip; i++) {
+  let randomNum = Math.round(Math.random())
+
+  if (randomNum === 0) {
+    console.log("Heads")
+  } else {
+    console.log("Tails")
+  }
+}
